a4bc4fc0fdee15f3c1d9200a41a7666400206711
RiffLesss <romachevif@gmail.com>
2020-05-07 20:53:53 +0300

    Data Base update

--- data/songs.py
+++ data/songs.py
@@ -5,7 +5,7 @@ from .db_session import SqlAlchemyBase
 
 
 class Song(SqlAlchemyBase, SerializerMixin):
-    __tablename__ = 'sounds'
+    __tablename__ = 'songs'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     album_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("products.id"))
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
